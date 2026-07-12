38509e3f1ca191f67fdcf4bad13e860525dba96b
amanmehta276 <166599351+amanmehta276@users.noreply.github.com>
2026-07-13 01:33:26 +0530

    new

---
+++ modu.py
@@ -0,0 +1,53 @@
+
+# a=10
+# a%=3
+
+# print(a)
+
+# for i in range(1,6):
+#     print(i,end=" ")
+#     for j in range(4):
+#         print(i**j,end=" ")
+#     print()
+# 1 1 1 1 1 
+# 2 1 2 4 8 
+# 3 1 3 9 27 
+# 4 1 4 16 64 
+# 5 1 5 25 125 
+
+# # height as a float 
+# print(float(172))
+# print(int(172))
+# a=""
+# # print(type(3+4j))
+
+# base=int(input("Enter base : "))
+# height=int(input("Enter height : "))
+# print(0.5*base*height)
+
+
+# a=int(input("Side a: "))
+# b=int(input("Enter b: "))
+# c=int(input("Enter c : "))
+
+# print(a*b*c)
+
+
+# x1=int(input("x1 : "))
+# x2=int(input("x2 : "))
+# y1=int(input("y1 : "))
+# y2=int(input("y2 : "))
+
+# print((y2-y1)//(x2-x1))
+
+# a="python"
+# b="dragn"
+
+# if "on" in a and "on" in b:
+#     print(True)
+# else:
+#     print(False)
+
+a=int(input("Enter your year : "))
+b=a*365*24*60*60
+print(f"You have lived {b} seconds")
---
+++ string.py
@@ -0,0 +1,41 @@
+# a="Aman "
+# b="Mehta "
+
+# print(a+b)
+# print(len(a+b))
+# print(len(a)>len(b))
+
+# print("Aman\tMehta")
+# print("Aman\\Mehta")
+
+# ab="Aman"
+# a,b,c,d=ab
+# print(a)
+
+
+# a="Aman"
+# print(a[0])
+
+# a="AmanMehta"
+# print(a[0:6:2])
+
+# a="aman\tMehta"
+# print(a.capitalize())
+# print(a.count('a'))
+# print(a.endswith('a'))
+# print(a.expandtabs(10))
+# print(a.find('a'))
+# print(a.rfind('a'))
+
+# a="Aman"
+# print(a.isalnum())
+
+# def num(web_tech):
+#     result= " ".join(web_tech)
+#     return result
+
+# web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
+# print(num(web_tech))
+
+challenge = 'thirty days of python'
+print(challenge.split())
