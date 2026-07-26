a58ee72a7b4b28b32d9a9318070777807289c347
amanmehta276 <166599351+amanmehta276@users.noreply.github.com>
2026-07-27 01:03:37 +0530

    new

---
+++ MatlPlotLib/1.matplot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# print(mpl.__version__)
+
+x=np.array([2023,2024,2025,2026])   #x-axis
+y1=np.array([15,25,30,20])   #y-axis
+y2=np.array([17,23,38,5])
+y3=np.array([13,15,20,5])
+
+line_style=dict(marker=".",markersize=10,markerfacecolor="cyan",markeredgecolor="cyan",
+         linestyle="solid",linewidth=1)
+
+plt.plot(x,y1,color="blue",**line_style)  #the plot of graph of x and y
+
+plt.plot(x,y2,color="red",**line_style)  #** unpacks the idictionary
+
+plt.plot(x,y3,color="green",**line_style)
+# plt.plot(x,y2)
+# plt.plot(y)
+# plt.plot(x)
+plt.show()  #show the plot graph
